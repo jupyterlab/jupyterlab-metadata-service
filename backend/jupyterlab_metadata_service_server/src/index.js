77fd6528514488d5eab05b7330d5a5be09e71d61
Ryan Henning <ryan@rhobota.com>
2019-02-07 11:47:08 -0600

    Playground should work through the Jupyter Server Proxy!

--- backend/jupyterlab_metadata_service_server/src/index.js
+++ backend/jupyterlab_metadata_service_server/src/index.js
@@ -24,6 +24,7 @@ const server = new ApolloServer({
   playground: {
     endpoint: '/metadata',
     settings: {
+      'request.credentials': 'same-origin',
       'editor.theme': 'dark'
     }
   }
