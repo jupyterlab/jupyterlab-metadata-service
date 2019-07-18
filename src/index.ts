dabf1cef3ffb818c1647c6ab42e9fcbebf2dd379
Saul Shanabrook <s.shanabrook@gmail.com>
2019-07-18 16:00:47 -0400

    Add linked data provider and registry

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin,
-  ILabShell
-} from "@jupyterlab/application";
+import { JupyterFrontEndPlugin } from "@jupyterlab/application";
 
-function activate(app: JupyterFrontEnd, labShell: ILabShell): void {
-  console.log("metadata!")
+import { flatten } from "jsonld";
+import { Token } from "@phosphor/coreutils";
+type LinkedData = {};
+
+export interface ILinkedDataProvider {
+  get(url: URL): LinkedData;
 }
+
+export class LinkedDataRegistry {
+  register(provider: ILinkedDataProvider): void {
+    this._providers.add(provider);
+  }
+
+  /**
+   * Lookup this URL in all providers and flatten the results.
+   */
+  get(url: URL): LinkedData {
+    return flatten([...this._providers].map(p => p.get(url)), null);
+  }
+
+  private _providers = new Set<ILinkedDataProvider>();
+}
+
+export const LinkedDataRegistryToken = new Token("LinkedDataRegistry");
 
 export default [
   {
-    activate,
-    id: "jupyterlab-metadata-service",
-    requires: [ILabShell],
+    activate: () => new LinkedDataRegistry(),
+    id: "jupyterlab-metadata-service:linked-data-registry",
+    requires: [],
+    provides: LinkedDataRegistryToken,
     autoStart: true
   }
 ] as JupyterFrontEndPlugin<any>[];
