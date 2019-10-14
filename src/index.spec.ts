36645aa48f22853c85c9c273efc5ac35e07299a8
Saul Shanabrook <s.shanabrook@gmail.com>
2019-10-14 19:32:32 -0400

    Try adding more tests to datasets

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -15,9 +15,26 @@ describe("JupyterLab", () => {
     await expect(page).toMatchElement("#jp-MainLogo");
   });
 
-  it("show be able to click on datasets", async () => {
+  it("show be able to show Dataset Explorer tab", async () => {
     expect.assertions(2);
     await expect(page).toClick('[title="Data Explorer"]');
     await expect(page).toMatch("Datasets");
+  });
+
+  it("should see files marker", async () => {
+    expect.assertions(1);
+    await expect(page).toMatchElement("h3", { text: "file:///" });
+  });
+
+  it("should be able to expand files", async () => {
+    expect.assertions(2);
+    const fileButtons = await page.$x('//button[../h3/text()="file:///"]');
+    await expect(fileButtons.length).toBe(1);
+    await expect(fileButtons[0]).toClick("button");
+  });
+
+  it("should see datasets.yml marker", async () => {
+    expect.assertions(1);
+    await expect(page).toMatch("datasets.yml");
   });
 });
