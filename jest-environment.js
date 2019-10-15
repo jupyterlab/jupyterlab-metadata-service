a40b3521a6c6542c255c2ec678cfab564fc04615
Saul Shanabrook <s.shanabrook@gmail.com>
2019-10-15 13:38:50 -0400

    Take screenshots on failure

---
+++ jest-environment.js
@@ -0,0 +1,30 @@
+// Copied from https://yarnpkg.com/en/package/@rws-air/jestscreenshot
+// jest-environment.ts
+
+const PuppeteerEnvironment = require("jest-environment-puppeteer");
+const JestScreenshot = require("@rws-air/jestscreenshot");
+require("jest-circus");
+
+// @ts-ignore
+class CustomEnvironment extends PuppeteerEnvironment {
+  async teardown() {
+    await this.global.page.waitFor(2000);
+    await super.teardown();
+  }
+
+  async handleTestEvent(event, state) {
+    if (event.name === "test_fn_failure") {
+      const testName = state.currentlyRunningTest.name;
+
+      const jestScreenshot = new JestScreenshot({
+        page: this.global.page,
+        dirName: __dirname,
+        testName
+      });
+
+      await jestScreenshot.setup();
+    }
+  }
+}
+
+module.exports = CustomEnvironment;
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ const { defaults: tsjPreset } = require("ts-jest/presets");
 module.exports = {
   rootDir: "src",
   preset: "jest-puppeteer",
+  
+  //  Needed for jest-screenshots
+  testRunner: 'jest-circus/runner',
+  
+  testEnvironment: '../jest-environment.js',
   transform: {
     ...tsjPreset.transform
   }
