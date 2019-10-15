e7536d7891b86dddafb7dd94abf947fb79a0609f
Saul Shanabrook <s.shanabrook@gmail.com>
2019-10-15 12:57:13 -0400

    Expand datasets as well

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,20 @@
+import { ElementHandle } from "puppeteer";
+
 const { setDefaultOptions } = require("expect-puppeteer");
 
 const timeout = 10 * 1000;
 
 jest.setTimeout(timeout);
 setDefaultOptions({ timeout });
+
+
+async function getXPath(xpath: string): Promise<ElementHandle<Element>> {
+  await page.waitForXPath(xpath)
+  const elements = await page.$x(xpath);
+  expect(elements.length).toBe(1)
+  return elements[0]
+}
+
 
 describe("JupyterLab", () => {
   beforeAll(async () => {
@@ -15,7 +26,7 @@ describe("JupyterLab", () => {
     await expect(page).toMatchElement("#jp-MainLogo");
   });
 
-  it("show be able to show Dataset Explorer tab", async () => {
+  it("show be able to show 'Data Explorer' tab", async () => {
     expect.assertions(2);
     await expect(page).toClick('[title="Data Explorer"]');
     await expect(page).toMatch("Datasets");
@@ -28,14 +39,31 @@ describe("JupyterLab", () => {
 
   it("should be able to expand files", async () => {
     expect.assertions(1);
-    await page.waitForXPath('//button[../h3/text()="file:///"]')
-    const filebuttons = await page.$x('//button[../h3/text()="file:///"]');
-    expect(filebuttons.length).toBe(1)
-    await filebuttons[0].click();
+    const filebutton = await getXPath('//button[../h3/text()="file:///"]');
+    await filebutton.click();
   });
 
   it("should see datasets.yml marker", async () => {
     expect.assertions(1);
     await expect(page).toMatchElement("h3", { text: "datasets.yml" });
   });
+
+
+  it("should be able to expand datasets.yml", async () => {
+    expect.assertions(1);
+    const datasetsButton = await getXPath('//button[../h3/text()="datasets.yml"]');
+    await datasetsButton.click();
+  });
+
+  it("should show datasets label", async () => {
+    expect.assertions(1);
+    await expect(page).toMatchElement("h3", { text: "A Publication" });
+  });
+
+  it("show be able to show 'Data Browser' tab", async () => {
+    expect.assertions(2);
+    await expect(page).toClick('[title="Data Browser"]');
+    await expect(page).toMatch("Follow active?");
+  });
+
 });
